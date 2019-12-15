a6746f9795d737485ea13c39adcaaea9adde6b86
itsezc <33750251+itsezc@users.noreply.github.com>
2019-12-15 17:21:10 +0000

    Fuck the governement

--- source/index.js
+++ source/index.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow, session } =  require('electron')
 const path = require ('path')
 
+// app.commandLine.appendSwitch('proxy-server', 'socks5://127.0.0.1:9050')
+
 let window
 
 require('electron-reload')('./*', {
@@ -9,13 +11,18 @@ require('electron-reload')('./*', {
 })
 
 let createWindow = () => {
+
 	let window = new BrowserWindow({
 		width: 800,
 		height: 600,
 		title: 'Iridium'
- 	})
+	 })
+	 
 	window.maximize()
-	window.loadURL('https://duckduckgo.com')
+	
+	window.loadURL(`file://${__dirname}/index.html`)
+
+	// window.loadURL('https://duckduckgo.com')
 	//window.webContents.openDevTools()
 
 	window.on('close', () => {
